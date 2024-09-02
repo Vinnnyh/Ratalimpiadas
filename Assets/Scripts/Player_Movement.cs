f651ea39bada56e86f7d0d9cb3ffb2366b929e80
Vinnnyh <116193876+Vinnnyh@users.noreply.github.com>
2024-09-02 12:54:26 -0500

    Cambios golpe PP
    Se cambio la manera en que se golpea la pelota de ping-pong siendo ahora que la fuerza depende del tiempo en que se mantiene presionada la tecla. (se cambiaron las teclas de E,F a la tecla espacio)

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -8,48 +8,44 @@ public class Player_Movement : MonoBehaviour
     float speed = 3f;
     
     bool hitting;
+    // Variable para medir el tiempo que se mantiene presionada la tecla
+    float hitChargeTime = 0f; 
 
     Vector3 aimTargetInitialPosition;
 
     ShotManager shotManager;
     Shot currentShot;
+
     private void Start()
     {
         aimTargetInitialPosition = aimTarget.position;
         shotManager = GetComponent<ShotManager>();
         currentShot = shotManager.topSpin;
     }
-    
 
     void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
         
-        // Detectar cuando se presiona la tecla F
-        if (Input.GetKeyDown(KeyCode.F))
+        // Detectar cuando se presiona la tecla de disparo (en este caso, espacio)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             hitting = true;
-            currentShot = shotManager.topSpin;
+            hitChargeTime = 0f; // Reiniciar el tiempo de carga al empezar a presionar la tecla
         }
 
-        // Detectar cuando se suelta la tecla F
-        if (Input.GetKeyUp(KeyCode.F))
+        // Detectar cuando se mantiene presionada la tecla de disparo
+        if (hitting)
+        {
+            hitChargeTime += Time.deltaTime; // Incrementar el tiempo de carga según el tiempo que la tecla esté presionada
+        }
+
+        // Detectar cuando se suelta la tecla de disparo
+        if (Input.GetKeyUp(KeyCode.Space))
         {
             hitting = false;
-        }
-
-        // Detectar cuando se presiona la tecla E
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            hitting = true;
-            currentShot = shotManager.flat;
-        }
-
-        // Detectar cuando se suelta la tecla E
-        if (Input.GetKeyUp(KeyCode.E))
-        {
-            hitting = false;
+            PerformShot(); // Llamar a la función para disparar
         }
 
         // Movimiento del aimTarget
@@ -65,6 +61,26 @@ public class Player_Movement : MonoBehaviour
         }
     }
 
+    // Función que realiza el disparo
+    private void PerformShot()
+    {
+        // Limitar el tiempo de carga para que no exceda un valor máximo
+        float maxChargeTime = 3f; // Tiempo máximo de carga
+        float chargePercent = Mathf.Clamp01(hitChargeTime / maxChargeTime);
+
+        // Fuerza base
+        float baseHitForce = 5f;
+        float baseUpForce = 2f;
+
+        // Calcular la fuerza final en función del tiempo de carga
+        float finalHitForce = baseHitForce + (currentShot.hitForce * chargePercent);
+        float finalUpForce = baseUpForce + (currentShot.upForce * chargePercent);
+
+        // Aplicar la fuerza calculada
+        currentShot.hitForce = finalHitForce;
+        currentShot.upForce = finalUpForce;
+    }
+
     // Detectar colisión con "Ball"
     private void OnTriggerEnter(Collider other)
     {
@@ -75,3 +91,4 @@ public class Player_Movement : MonoBehaviour
         }
     }
 }
+
