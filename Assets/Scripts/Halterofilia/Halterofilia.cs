62076983dd50a098e7520cd9b4daecfce1cf9b28
WJ4ck <ccaballero768@unab.edu.co>
2024-10-03 11:35:55 -0500

    Alpha 0.2
    Se agregaron estados de animacion controlables

--- Assets/Scripts/Halterofilia/Halterofilia.cs
+++ Assets/Scripts/Halterofilia/Halterofilia.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,7 +10,6 @@ public class Halterofilia : MonoBehaviour
 {
     public Animator animator; // Referencia al componente Animator
     private int keyPressCount = 0; // Contador de pulsaciones de tecla
-    private int state = 0; // Estado de animación
     private int level = 1; //Nivel en el que esta
     private int difficulty = 1;
 
@@ -18,7 +18,7 @@ public class Halterofilia : MonoBehaviour
     private Double remainingTime = 0;
 
     void Update()
-    {   
+    {
 
         if (Mathf.Ceil((float)remainingTime) == 40)
         {
@@ -51,66 +51,19 @@ public class Halterofilia : MonoBehaviour
 
         pulsaciones(difficulty);
 
+        animator.SetInteger("Counter", (int)remainingTime);
+        //isReversed = animator.GetBool("Reverse");
+        
+
         // Verificar si se presiona la tecla Espacio
         if (Input.GetKeyDown(KeyCode.Space))
         {
             keyPressCount++;
             remainingTime++;
             Debug.Log("Pulsaciones: " + remainingTime);
-
-            // Cambiar estado basado en el número de pulsaciones
-            if (remainingTime >= 30) //Estado de pesa levantada totalmente
-            {
-                state = 3;
-            }
-            else if (remainingTime < 30 || remainingTime > 20) //Estado de pesa en el pecho
-            {
-                state = 2;
-            }
-            else if (remainingTime >= 20) //Estado de pesa en el pecho
-            {
-                state = 2;
-            }
-            else if (remainingTime >= 10) //Estado de pesa recien levantada
-            {
-                state = 1;
-            }
-
-            CambiarAnimacion();
         }
     }
-
-    // Método para cambiar la animación basada en el estado
-    void CambiarAnimacion()
-    {
-        switch (state)
-        {
-            case 1:
-                animator.Play("Pose 1");
-                break;
-            case 2:
-                animator.Play("Pose 2");
-                break;
-            case 3:
-                animator.Play("Pose 3");
-                break;
-            case 4:
-                animator.speed = -1f;
-                animator.Play("Pose 1");
-                animator.speed = 1f;
-                break;
-            case 5:
-                animator.speed = -1f;
-                animator.Play("Pose 2");
-                animator.speed = 1f;
-                break;
-            case 6:
-                animator.speed = -1f;
-                animator.Play("Pose 3");
-                animator.speed = 1f;
-                break;
-        }
-    }
+   
 
     void pulsaciones(int difficulty)
     {
